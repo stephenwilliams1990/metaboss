9a79febdf52574434fb67d3890c13dc4b648c7b7
stephenwilliams1990 <stephenwilliams1990@gmail.com>
2021-11-26 12:36:06 -0500

    test script

---
+++ test.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+
+const updateAuthority = "EVbtsbmo7Av16n4ruYQLNTmCbTdcSRwNZbMsCZeCvGg8"
+
+fs.readFile(`./snapshot/${updateAuthority}_holders.json`, 'utf8', function(err, data){
+            
+    const json = JSON.parse(data);
+
+    tokens = json.length
+
+    let a = []
+
+    for (let i=0; i < json.length; i++){
+        a.push(json[i].owner_wallet)
+    }
+
+    unique = a.filter((item, i, ar) => ar.indexOf(item) === i)
+
+    console.log('Tokens', tokens, 'Holders', unique)
+});
